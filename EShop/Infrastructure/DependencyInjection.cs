09fff11a4db05e3095c38922ae8ea34db3bd4f94
Mohammad Hossein <mhosein8080@gmail.com>
2023-02-11 10:33:01 +0330

    init commit

---
+++ EShop/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using EShop.Infrastructure.Persistance;
+
+namespace EShop.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(opt => opt.UseNpgsql(configuration.GetConnectionString("Postgresql")));
+
+        return services;
+    }
+}
